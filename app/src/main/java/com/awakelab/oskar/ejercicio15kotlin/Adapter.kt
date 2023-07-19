07dbe0986399f9058653de72f577c959ce7b3c94
OskarUno <oorellana80@gmail.com>
2023-07-19 01:55:41 -0400

    Ejercicio 15, se agrega imagen en listado

--- app/src/main/java/com/awakelab/oskar/ejercicio15kotlin/Adapter.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio15kotlin/Adapter.kt
@@ -1,15 +1,23 @@
 package com.awakelab.oskar.ejercicio15kotlin
 
+
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.awakelab.oskar.ejercicio15kotlin.databinding.ItemLayoutBinding
+import com.bumptech.glide.Glide
+
 
 class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
     var listaPokemon = mutableListOf<Pokemon>()
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemLayoutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -27,10 +35,20 @@ class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
     }
 
     class ViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        private var imageView: ImageView? = null
         fun bind(pokemon: Pokemon) {
             binding.tvNombre.text = pokemon.nombre
             binding.tvTipo.text = pokemon.tipo
+            binding.imageView.let {
+                Glide.with(it)
+                    .load(pokemon.imgUrl)
+                    .into(binding.imageView)
+            }
+
+
         }
 
+
     }
+
 }
